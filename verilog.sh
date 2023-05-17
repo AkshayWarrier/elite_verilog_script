ef59dd99e30b261342cd629eec050ab895772d27
Rohith <rohithgeorgeninan@gmail.com>
2023-05-17 12:55:53 +0530

    Add flag to open gtkwave

--- verilog.sh
+++ verilog.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
 # Script to compile verilog using iverilog and visualize using gtkwave
-module_name=$1 
+
+if [[ "$1" == -* ]]; then
+    if [[ "$1" ==  "-v" ]]; then
+        module_name=$2
+        RUN_GTKWAVE=true
+    else
+        echo "Invalid option"
+        exit 1
+    fi
+else
+    module_name=$1
+    RUN_GTKWAVE=false
+fi
+
 cd $module_name
 ls *.v > filelist.txt
 iverilog -o $module_name -c filelist.txt
 ./$module_name
-gtkwave dump.vcd &> /dev/null &
+
+if [[ "$RUN_GTKWAVE" == true ]]; then
+    gtkwave *.vcd &> /dev/null &
+fi
+
 cd ..
